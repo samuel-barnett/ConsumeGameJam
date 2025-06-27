59fee6260b512610ba82376ac5131e0d3c52489e
Samuel Barnett <samuel.barnett@mymail.champlain.edu>
2025-06-27 00:43:20 -0400

    camera rotation setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 
 public class PlayerController : Entity
 {
-    // 
+    // states 
     bool canJump;
-
+    bool canRotateCamera;
 
     // Serializes
+    [Header("Refs")]
     [SerializeField] GameObject cameraRoot;
 
-
+    [Header("Adjustable Values")]
     [SerializeField] float movementSpeed = 5;
     [SerializeField] float jumpForce = 1;
-
+    [SerializeField] AnimationCurve cameraRotation;
 
     protected override void SetupEntity()
     {
         cameraRoot.transform.SetParent(null);
+        canRotateCamera = true;
     }
 
     // Update is called once per frame
@@ -34,11 +37,13 @@ public class PlayerController : Entity
         float moveSide = Input.GetAxisRaw("Horizontal");
         float moveForward = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = cameraRoot.transform.worldToLocalMatrix * new Vector3(moveSide, 0, moveForward).normalized;
-        rb.AddForce(move * movementSpeed);
+        Vector3 moveVector = ((cameraRoot.transform.right * moveSide) + (cameraRoot.transform.forward * moveForward)).normalized;
+
+        //Vector3 move = cameraRoot.transform.worldToLocalMatrix * new Vector3(moveSide, 0, moveForward).normalized;
+        rb.AddForce(moveVector * movementSpeed);
 
         // look in the direction of movement
-        transform.LookAt(transform.position + move);
+        transform.LookAt(transform.position + moveVector);
     }
 
     // trigger hits or leaves floor
@@ -48,7 +53,7 @@ public class PlayerController : Entity
     }
     private void OnTriggerExit(Collider other)
     {
-        canJump = false;
+        //canJump = false;
     }
 
     void Jump()
@@ -57,7 +62,7 @@ public class PlayerController : Entity
         {
             //Debug.Log("jump");
             rb.AddForce(transform.up * jumpForce);
-
+            canJump = false;
         }
     }
 
@@ -66,16 +71,40 @@ public class PlayerController : Entity
         cameraRoot.transform.position = transform.position;
 
         // rotate left
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && canRotateCamera)
         {
-            cameraRoot.transform.Rotate(Vector3.up * 90);
+            StartCoroutine(RotateCamera(true));
+            //cameraRoot.transform.Rotate(Vector3.up * 90);
         }
 
         // rotate right
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && canRotateCamera)
         {
-            cameraRoot.transform.Rotate(Vector3.down * 90);
+            StartCoroutine(RotateCamera(false));
+            //cameraRoot.transform.Rotate(Vector3.down * 90);
         }
+    }
+
+    IEnumerator RotateCamera(bool left)
+    {
+        canRotateCamera = false;
+        Vector3 rotationVector = (left) ? Vector3.up : Vector3.down;
+        Quaternion originalRotation = cameraRoot.transform.rotation;
+
+        float timer = 0;
+        float currentEvaluation;
+        while (timer < 0.25)
+        {
+            timer += Time.fixedDeltaTime;
+            currentEvaluation = cameraRotation.Evaluate(timer);
+
+            cameraRoot.transform.rotation = originalRotation;
+            cameraRoot.transform.Rotate(rotationVector * currentEvaluation);
+
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+
+        canRotateCamera = true;
     }
 
 
